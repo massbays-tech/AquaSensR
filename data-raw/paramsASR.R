0a42501fbd2d365116e831b060733b44c4c94b66
fawda123 <mbafs2012@gmail.com>
2026-02-08 20:11:45 -0500

    update paramsASR for consistent label column notation

--- data-raw/paramsASR.R
+++ data-raw/paramsASR.R
@@ -1,3 +1,8 @@
-paramsASR <- readxl::read_excel('inst/extdata/ParameterMapping.xlsx')
+library(dplyr)
+
+paramsASR <- readxl::read_excel('inst/extdata/ParameterMapping.xlsx') |> 
+  rename(
+    Label = `MassWateR Column Label`
+  )
 
 usethis::use_data(paramsASR, overwrite = TRUE)
